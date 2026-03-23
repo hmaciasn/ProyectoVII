8a7caf077881a03157161c551deefc00a4add493
hmaciasn <144642944+hmaciasn@users.noreply.github.com>
2026-03-22 22:43:58 -0600

    Add product fetching and rendering logic
    Se añade codigo para front end

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,26 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+const contenedor = document.getElementById('productos');
+
+if (contenedor) {
+  fetch('/productos-json')
+    .then(res => res.json())
+    .then(productos => {
+      console.log(productos);
+
+      contenedor.innerHTML = "";
+
+      productos.forEach(p => {
+        contenedor.innerHTML += `
+          <div class="border p-4 rounded shadow">
+            <h3>${p.name}</h3>
+            <p>${p.description ?? ''}</p>
+            <strong>$${p.price}</strong>
+          </div>
+        `;
+      });
+    })
+    .catch(err => console.error("ERROR:", err));
+}
+
